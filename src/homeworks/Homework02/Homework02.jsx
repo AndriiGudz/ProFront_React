85cb9b560547ef458351332c540a4203cf810eb5
Andrii Gudz <andrey.gudz@gmail.com>
2024-05-29 17:24:05 +0200

    homework-02 udate

--- src/homeworks/Homework02/Homework02.jsx
+++ src/homeworks/Homework02/Homework02.jsx
@@ -2,7 +2,11 @@ import ProfileCard from "../../components/ProfileCard/ProfileCard"
 import './styles.css'
 
 function Homework02() {
-  return <div className="homework-02"><ProfileCard /></div>
+  return <div className="homework-02">
+    <ProfileCard />
+    <ProfileCard />
+    <ProfileCard />
+    </div>
 }
 
 export default Homework02
