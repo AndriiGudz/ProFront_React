efadf98bb2bb195eb9daa7760563dccc6887c9ac
Andrii Gudz <andrey.gudz@gmail.com>
2024-05-29 21:04:54 +0200

    lesson_03

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 
 //Lessons
 import Lesson02 from './lessons/Lesson02/Lesson02'
+import Lesson03 from './lessons/Lesson03/Lesson03'
 
 //Homeworks
 import Homework02 from './homeworks/Homework02/Homework02'
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className="App">
       {/* <Lesson02 /> */}
-      <Homework02 />
+      <Lesson03 />
+      {/* <Homework02 /> */}
     </div>
   )
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,31 +1,45 @@
 import './styles.css'
 
-function Button() {
-  //   const buttonName = 'Get data'
-  const sendButton = 'Send'
-  const getButton = 'Get'
-  const isGetButton = false
-  const buttonClass = 'button-component'
+// function Button() {
+//   const buttonName = 'Get data'
+// Лекция 2
+// const sendButton = 'Send'
+// const getButton = 'Get'
+// const isGetButton = false
+// const buttonClass = 'button-component'
 
-  // Вариант 1
-  //   return <button className={buttonClass}>{buttonName}</button>
+// Вариант 1
+//   return <button className={buttonClass}>{buttonName}</button>
 
-  //   Вариант 2
-  //   return (
-  //     <div>
-  //       <h2>Button</h2>
-  //       <button className={buttonClass}>
-  //         {isGetButton ? getButton : sendButton}
-  //       </button>
-  //     </div>
-  //   )
+//   Вариант 2
+//   return (
+//     <div>
+//       <h2>Button</h2>
+//       <button className={buttonClass}>
+//         {isGetButton ? getButton : sendButton}
+//       </button>
+//     </div>
+//   )
 
-  // Вариант 3
-  return (
-    <button className={buttonClass}>
-      {isGetButton ? getButton : sendButton}
-    </button>
-  )
+// Вариант 3
+// return (
+//   <button className={buttonClass}>
+//     {isGetButton ? getButton : sendButton}
+//   </button>
+// )
+
+// }
+
+// Лекция 3
+// Вариант 4
+
+// function Button(props)
+function Button({name = 'Get', isPrimary = true}) {
+  const buttonClass = isPrimary ? 'primary-buttom' : 'delete-buttom'
+  // const buttonClass = props.isPrimary ? 'primary-buttom' : 'delete-buttom'
+  // console.log(props);
+
+  return <button className={`button-component ${buttonClass}`}>{name}</button>
 }
 
 export default Button
